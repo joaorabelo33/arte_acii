0712a21e4b1341d205ffaacd44db0ba6deb80ae6
joaorabelo33 <143022690+joaorabelo33@users.noreply.github.com>
2023-08-27 13:05:55 -0300

    Add files via upload

---
+++ 01_arte_em_ASCII.py
@@ -0,0 +1,77 @@
+print("Oi, tudo bem?")
+print("Estou com fome!")
+print("Uma imagem de cachorro...")
+print(" 0_____ ")
+print("  ||||  ")
+print(''' Uma imagem de cachorro...
+    0_____
+     ||||
+''')
+
+print("Meu animal favorito é a ovelha:")
+print('''
+  o-###-
+    | |    #
+    ''')
+
+print("Eu moro em São Paulo")
+print('''
+    _|_
+   |   |
+   |#  |____
+   |   |    |
+   |  #|  # |
+   |   | #  |
+
+   ''')
+
+print("Calculando texto")
+print(" Olá" * 5)
+
+print("Olá," + "tudo bem?")
+
+print("ha" * 4)
+print("ba" + "na"*2)
+print("Bra" + "sil" + "!"*10)
+
+
+
+print("#"*30)
+print("#"*30)
+
+
+print("/\  " *10)
+print("  \/" *10)
+print("Aqui vai um dinossauro!")
+
+print('''
+                    ___
+                   /   )
+            .-^^^-/   -
+         __/         /
+       < __.|__|-|__|
+
+
+''')
+print(''' E aqui vai um bolo! hummmmmmmmmmm
+
+
+ . . . . . . . . . .
+ i i i i i i i i i i
+####################
+====================
+####################
+====================
+####################
+
+''')
+
+
+
+
+
+
+
+
+
+
